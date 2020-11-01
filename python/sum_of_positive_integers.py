11db0d171457850a6841c7d5cb217e03f8c2088c
Wicked <wwicked@o2.pl>
2020-11-01 19:29:10 +0100

    Add sum_of_positive_integers

---
+++ python/sum_of_positive_integers.py
@@ -0,0 +1,11 @@
+import re
+
+
+def positive_sum(s: str) -> int:
+    ints = [int(i) for i in re.findall("-?\d{1,}", s) if int(i) >= 1]
+
+    return sum(ints)
+
+
+print(positive_sum("-12#-33 13%14&-11"))
+print(positive_sum("22 13%14&-11-22 13 12 0"))
