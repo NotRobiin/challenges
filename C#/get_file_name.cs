9f730114897a539121ae95c0cfb8aaa26c8566ef
Wicked <wwicked@o2.pl>
2020-11-01 19:15:32 +0100

    Add creating_a_counter

--- C#/get_file_name.cs
+++ C#/get_file_name.cs
@@ -1,9 +1,9 @@
 public class Program
 {
-	public static string GetFilename(string path)
-	{
+    public static string GetFilename(string path)
+    {
         string[] sub = path.Split('/');
 
         return sub[sub.Length - 1];
-	}
+    }
 }
---
+++ python/creating_a_counter.py
@@ -0,0 +1,6 @@
+def counter():
+    counter.count += 1
+    return counter.count - 1
+
+
+counter.count = 0
